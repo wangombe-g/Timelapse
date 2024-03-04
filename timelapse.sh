c8f22c1c4dde445b2e32334edacedc76737f8f87
Wangombe <ewangombe-ctr@wikimedia.org>
2024-03-04 19:14:12 +0300

    Specify /mnt/timelapse directory
    * Some optimizations

--- timelapse.sh
+++ timelapse.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-# Define variables
-
-if [ $# -eq 0 ]; then
-  echo "Please specify the duration in hours as a float."
+# Check if the required arguments were provided
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <duration in hours> <interval in seconds>"
+  echo "Example: $0 1.5 30"
   exit 1
 fi
 
 # Capture input duration in hours and convert it to seconds
 DURATION_HOURS=$1
+INTERVAL=$2
 DURATION_SECONDS=$(echo "$DURATION_HOURS * 3600" | bc)
 
 SAVE_PATH="/home/eugene/Pictures/Timelapse/timelapse" # Update this path where you want to save the images
-TMP_PATH="/mnt/mytmpfs"  # Temporary storage in RAM
-INTERVAL=20 # Capture interval in seconds
+TMP_PATH="/mnt/timelapse"  # Temporary storage in RAM
 
 # Calculate the number of captures based on duration and interval
 NUM_CAPTURES=$(echo "$DURATION_SECONDS / $INTERVAL" | bc)
 
-# Create a temporary directory for capturing images
-mkdir -p "$TMP_PATH"
+TIMELAPSE_DURATION=$(echo "$NUM_CAPTURES / 30" | bc)
+
+echo "Capturing $NUM_CAPTURES images over $DURATION_HOURS hours with $INTERVAL seconds interval"
+echo "Timelapsed video duration will be $TIMELAPSE_DURATION seconds."
 
 # Start capturing images
 for ((i=0; i<NUM_CAPTURES; i++)); do
   TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
   FINAL_FILENAME="$SAVE_PATH/image_$TIMESTAMP.jpeg"
 
-  streamer -o $TMP_PATH/tmp_image_0.jpeg -c /dev/video4 -s 1920x1080 -j 100 -t 5 -r 1
+  streamer -o $TMP_PATH/tmp_image_0.jpeg -c /dev/video4 -s 1920x1080 -j 100 -t 5 -r 1 -q
 
 	# Copy the last image to the final location
   cp "$TMP_PATH/tmp_image_4.jpeg" "$FINAL_FILENAME"
